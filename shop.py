cded72f8b57c94cbe7d2fd3e5c90b88b3bb96707
RichardY42 <RichardY42@nycstudents.net>
2025-12-19 10:16:21 -0500

    ad

---
+++ shop.py
@@ -0,0 +1,23 @@
+weapontype=[
+    {
+        "name": "rusty sword",
+        "physdmg": 3,
+        "type": "sword"
+    },
+    {
+        "name": "rusty gaunlet",
+        "physdmg": 4,
+        "type": "fists"
+    },
+    {
+        "name": "basic staff",
+        "physdmg": 1,
+        "magidmg": 5,
+        "type": "staff"
+    }
+]
+#classes knight fighter mage
+
+class shop:
+    def weaponshop(self, money, inv, weapon):
+        
