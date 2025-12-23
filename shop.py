019ce37dce049fffda9dc91ef7d70f30a7f00555
RichardY42 <RichardY42@nycstudents.net>
2025-12-23 09:53:14 -0500

    x

--- shop.py
+++ shop.py
@@ -1,34 +1,61 @@
 weapontype=[
     {
         "name": "rusty sword",
-        "physdmg": 3,
+        "phydmg": 3,
+        "mgcdmg": 0,
+        "rngdmg": 0,
         "type": "sword"
     },
     {
         "name": "rusty gaunlet",
         "physdmg": 4,
+        "mgcdmg": 0,
+        "rngdmg": 0,
         "type": "fists"
     },
     {
         "name": "basic staff",
-        "physdmg": 1,
-        "magidmg": 5,
+        "physdmg": 0,
+        "mgcdmg": 5,
+        "rngdmg": 3,
         "type": "staff"
+    }
+]
+resources=[
+    {
+    "name": "Iron",
+    "amount": 1,
+    "type": "Metal",
+    },
+    {
+    "name": "Wood",
+    "amount": 1,
+    "type": "Wood",
+    },
+    {
+    "name": "Copper",
+    "amount": 1,
+    "type": "Metal",
     }
 ]
 testinv=[]
 #classes: knight fighter mage
 
-class shop:
+class shop(Class):
+    def genstore(phydmg, rngdmg, mgcdmg, inv):
+        later
     def weaponshop(inv):
         print("Buy a weapon... but choose wisely:")
         for index, i in enumerate(weapontype, start=1):
             print(f"{index}. {i}")
         choice=int(input("type the number: "))
         testinv.append(weapontype[choice-1])
+        self.phydmg+=weapontype[choice-1]['phydmg']
+        self.rngdmg+=weapontype[choice-1]['rngdmg']
+        self.mgcdmg+=weapontype[choice-1]['mgcdmg']
         print(f"Current Inventory: {testinv}")
     weaponshop(testinv)
 
     def smithy(inv):
-        if self.inv:
+        startingweapon=inv[1]
         print("if you wanna upgrade your current weapon you will need ")
