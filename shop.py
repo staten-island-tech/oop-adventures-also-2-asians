f8d511a3a5186c298fcfd8834be4d6cb61dd8168
RichardY42 <RichardY42@nycstudents.net>
2026-01-05 10:11:38 -0500

    ahuhauhhuhaahahahhauahuah

--- shop.py
+++ shop.py
@@ -1,4 +1,4 @@
-import 
+from character import Class
 
 weapontype=[
     {
@@ -40,10 +40,8 @@ resources=[
     "type": "Metal",
     }
 ]
-inv=[]
-currentequip=[]
-#classes: knight fighter mage
 
+#classes: knight fighter mage
 
 
 #I FORGOT HOW TO GET THE [CLASS] class FROM CHARACTER.PY AHHHHHHHHHH
@@ -52,19 +50,23 @@ class shop(Class):
     def __init__(self, name, money, hp, phydmg, rngdmg, mgcdmg, mana, inv, race, classs, equipped):
         super().__init__(self, name, money, hp, phydmg, rngdmg, mgcdmg, mana, inv, race, classs)
         self.equipped=equipped
+    invlist=[]
+    currentequip=[]
     def weaponshop(self):
+        self.inv=invlist
+        self.equipped=currentequip
         print("Buy a weapon... but choose wisely:")
         for index, i in enumerate(weapontype, start=1):
             print(f"{index}. {i}")
         choice=int(input("type the number: "))
         #subtract the previously chosen weapon stats from character stats
-        inv.append(weapontype[choice-1])
+        self.inv.append(weapontype[choice-1])
         self.phydmg+=weapontype[choice-1]['phydmg']
         self.rngdmg+=weapontype[choice-1]['rngdmg']
         self.mgcdmg+=weapontype[choice-1]['mgcdmg']
-        print(f"Current Inventory: {testinv}")
+        print(f"Current Inventory: {self.inv}")
     
 
     def smithy(inv):
-        startingweapon=inv[]
+        startingweapon=
         print("if you wanna upgrade your current weapon you will need ")
