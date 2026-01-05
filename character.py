249141efcef95e5e57b9d1a27a6cfc6b0b691e9f
Nathan-Zheng1 <nathanz32@nycstudents.net>
2026-01-05 10:01:34 -0500

    Revert "stats new file"
    This reverts commit 0ed4881296b00e1231a457b061834551f4cc7730.

---
+++ character.py
@@ -0,0 +1,80 @@
+#ryu0702
+
+class Class:
+    def __init__(self, name, money, hp, phydmg, rngdmg, mgcdmg, mana, inv, race, classs):
+        self.name = name
+        self.money = money
+        self.hp = hp
+        self.phydmg = phydmg
+        self.rngdmg = rngdmg
+        self.mgcdmg = mgcdmg
+        self.mana = mana
+        self.inv = inv
+        self.race = race
+        self.classs = classs
+    def human(self):
+        self.money += 24
+        self.hp += 99
+        self.mana += 10
+        self.race.append("Human")
+    def demon(self):
+        self.money += 9
+        self.hp += 74
+        self.phydmg += 1
+        self.mana += 5
+        self.race.append("Demon")
+    def angel(self):
+        self.money += 9
+        self.hp += 99
+        self.mgcdmg += 1
+        self.mana += 15
+        self.race.append("Angel")
+    def dwarf(self):
+        self.money += 24
+        self.hp += 74
+        self.phydmg -= 0.5
+        self.mana += 5
+        self.race.append("Dwarf")
+
+    #Races
+
+    def Warrior(self):
+        self.phydmg += 1
+        self.classs.append("Warrior")
+
+    def Archer(self):
+        self.rngdmg += 1
+
+    def Mage(self):
+        self.mgcdmg += 1
+        self.mana += 5
+        
+self = Class("John", 1, 1, 1, 0, 0, 0, [], [], [])
+
+while True:
+    user_input = input("Please choose a race!: Human, Demon, Angel, Dwarf - Remember you will not be able to change these later on. Keep in mind each race will have their own buffs/debuffs. If you would like to get info about these races, please type 'info'")
+    if user_input == "info":
+        print("The human class, the jack of all trades - master of none. This race is capable of becoming all of the classes, and poses no debuffs. All stats of this race are default. The demon class is a master of combat, specializing in close ranged physical fights. The demon class has high damage and a special ability, but sacrifices some health for it. Demon is incapable of using ranged weapons. The angel race is similar to the human race, but has a few passives and abilities. The angel is capable of using its wings to dodge attacks and is eligible to use all types of weapons and is stronger with magic type attacks. The dwarf race specializes in ranged combat, being small enough to evade attacks. The dwarf class is weaker with physical weapons, and stronger with ranged weapons.")
+    elif user_input == "Human":
+        self.human()
+        break
+    elif user_input == "Demon":
+        self.demon()
+        break
+    elif user_input == "Angel":
+        self.angel()
+        break
+    elif user_input == "Dwarf":
+        self.dwarf()
+        break
+    else:
+        print("Answer not reconized, please try again.")
+
+while True:
+    user_input = input("Please choose a class for your character, remember that some classes wont be capable of becoming specific classes.")
+    if user_input == "Warrior":
+        self.Warrior()
+    elif user_input == "Archer" and self.race != ["Demon"]:
+        self.Archer()
+    elif user_input == "Mage" and self.race != ["Demon"]:
+        self.Mage()
