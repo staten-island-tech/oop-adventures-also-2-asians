31947d4c83da68c0b9bcc6b59753229feb743781
Nathan-Zheng1 <nathanz32@nycstudents.net>
2026-01-05 10:11:57 -0500

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,7 @@
+from character import Class
+class Stats(Class):
+    def __init__(self, strength, defense, durability, mana):
+        self.strength = strength
+        self.defense = defense
+        self.durability = durability
+        self.mana = mana
