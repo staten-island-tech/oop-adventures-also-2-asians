74eca617d588e896d96e5e683cc8da34c58c8bbc
Nathan-Zheng1 <nathanz32@nycstudents.net>
2026-01-06 10:01:29 -0500

    working on the main function of the game

---
+++ mobs.py
@@ -0,0 +1,20 @@
+mobs = [
+    {
+    "name" : "bandit",
+    "damage" : 4,
+    "health" : 25,
+    "def" : 0
+},
+{
+    "name" : "rouge",
+    "damage" : 6,
+    "health" : 20,
+    "def" : 0
+},
+{
+    "name" : "Outcast",
+    "damage" : 2,
+    "health" : 10,
+    "def" : -1
+}
+    ]
