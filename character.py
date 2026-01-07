1d9a53fa7022529a683577e9ec89aa150ae68574
RichardY42 <RichardY42@nycstudents.net>
2026-01-07 09:54:48 -0500

    shop appear

--- character.py
+++ character.py
@@ -1,4 +1,46 @@
 
+weapontype=[
+    {
+        "name": "rusty sword",
+        "phydmg": 3,
+        "mgcdmg": 0,
+        "rngdmg": 0,
+        "type": "sword"
+    },
+    {
+        "name": "rusty gaunlet",
+        "physdmg": 4,
+        "mgcdmg": 0,
+        "rngdmg": 0,
+        "type": "fists"
+    },
+    {
+        "name": "basic staff",
+        "physdmg": 0,
+        "mgcdmg": 5,
+        "rngdmg": 3,
+        "type": "staff"
+    }
+]
+resources=[
+    {
+    "name": "Iron",
+    "amount": 1,
+    "type": "Metal",
+    },
+    {
+    "name": "Wood",
+    "amount": 1,
+    "type": "Wood",
+    },
+    {
+    "name": "Copper",
+    "amount": 1,
+    "type": "Metal",
+    }
+]
+
+
 class Class:
     def __init__(self, name, money, hp, phydmg, rngdmg, mgcdmg, mana, inv, race, classs):
         self.name = name
@@ -47,7 +89,34 @@ class Class:
     def Mage(self):
         self.mgcdmg += 1
         self.mana += 5
-        
+
+class shop(Class):
+    def __init__(self, equipped):
+        super().__init__(self)
+        self.equipped=equipped
+        self= shop([])
+    def weaponshop(self):
+        print("Buy a weapon... but choose wisely:")
+        for index, i in enumerate(weapontype, start=1): 
+            print(f"{index}. {i}")
+        choice=int(input("type the number: "))
+        #subtract the previously chosen weapon stats from character stats
+        self.inv.append(weapontype[choice-1])
+        self.phydmg+=weapontype[choice-1]['phydmg']
+        self.rngdmg+=weapontype[choice-1]['rngdmg']
+        self.mgcdmg+=weapontype[choice-1]['mgcdmg']
+        print(f"Current Inventory: {self.inv}")
+
+    def smithy(self):
+        upgradecounter=0
+        if self.equipped['name']==weapontype[0]['name']:
+            print("if you wanna upgrade your current weapon you will need enough resources: 10 metal")
+    #def genstore():
+
+
+
+
+
 self = Class("John", 1, 1, 1, 0, 0, 0, [], [], [])
 
 while True:
@@ -85,78 +154,6 @@ while True:
         break
 
 #_________________________________________________________________________________________
-weapontype=[
-    {
-        "name": "rusty sword",
-        "phydmg": 3,
-        "mgcdmg": 0,
-        "rngdmg": 0,
-        "type": "sword"
-    },
-    {
-        "name": "rusty gaunlet",
-        "physdmg": 4,
-        "mgcdmg": 0,
-        "rngdmg": 0,
-        "type": "fists"
-    },
-    {
-        "name": "basic staff",
-        "physdmg": 0,
-        "mgcdmg": 5,
-        "rngdmg": 3,
-        "type": "staff"
-    }
-]
-resources=[
-    {
-    "name": "Iron",
-    "amount": 1,
-    "type": "Metal",
-    },
-    {
-    "name": "Wood",
-    "amount": 1,
-    "type": "Wood",
-    },
-    {
-    "name": "Copper",
-    "amount": 1,
-    "type": "Metal",
-    }
-]
-
-#classes: knight fighter mage
-
-#
-
-#I FORGOT HOW TO GET THE [CLASS] class FROM CHARACTER.PY AHHHHHHHHHH
-
-class shop():
-    def __init__(self, equipped):
-        super().__init__(self)
-        self.equipped=equipped
-        self= shop([])
-    def weaponshop(self):
-        print("Buy a weapon... but choose wisely:")
-        for index, i in enumerate(weapontype, start=1): 
-            print(f"{index}. {i}")
-        choice=int(input("type the number: "))
-        #subtract the previously chosen weapon stats from character stats
-        self.inv.append(weapontype[choice-1])
-        self.phydmg+=weapontype[choice-1]['phydmg']
-        self.rngdmg+=weapontype[choice-1]['rngdmg']
-        self.mgcdmg+=weapontype[choice-1]['mgcdmg']
-        print(f"Current Inventory: {self.inv}")
-
-    def smithy(self):
-        upgradecounter=0
-        if self.equipped['name']==weapontype[0]['name']:
-            print("if you wanna upgrade your current weapon you will need enough resources: 10 metal")
-    #def genstore():
-
-
-
 while True:
     user_input = input("x")
     if user_input== "x":
