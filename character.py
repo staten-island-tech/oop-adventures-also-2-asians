85d1e57015d25dd1cf79f8f42fd4262a37dab69e
RichardY42 <RichardY42@nycstudents.net>
2026-01-07 09:33:47 -0500

    AHHH

--- character.py
+++ character.py
@@ -1,4 +1,3 @@
-#ryu0702
 
 class Class:
     def __init__(self, name, money, hp, phydmg, rngdmg, mgcdmg, mana, inv, race, classs):
@@ -84,3 +83,74 @@ while True:
         self.Mage()
         print("Class Chosen: Mage")
         break
+
+weapontype=[
+    {
+        "name": "rusty sword",
+        "phydmg": 3,
+        "mgcdmg": 0,
+        "rngdmg": 0,
+        "type": "sword"
+    },
+    {
+        "name": "rusty gaunlet",
+        "physdmg": 4,
+        "mgcdmg": 0,
+        "rngdmg": 0,
+        "type": "fists"
+    },
+    {
+        "name": "basic staff",
+        "physdmg": 0,
+        "mgcdmg": 5,
+        "rngdmg": 3,
+        "type": "staff"
+    }
+]
+resources=[
+    {
+    "name": "Iron",
+    "amount": 1,
+    "type": "Metal",
+    },
+    {
+    "name": "Wood",
+    "amount": 1,
+    "type": "Wood",
+    },
+    {
+    "name": "Copper",
+    "amount": 1,
+    "type": "Metal",
+    }
+]
+
+#classes: knight fighter mage
+
+#
+
+#I FORGOT HOW TO GET THE [CLASS] class FROM CHARACTER.PY AHHHHHHHHHH
+
+class shop(Class):
+    def __init__(self, equipped):
+        super().__init__(self)
+        self.equipped=equipped
+    def weaponshop(self):
+        print("Buy a weapon... but choose wisely:")
+        for index, i in enumerate(weapontype, start=1): 
+            print(f"{index}. {i}")
+        choice=int(input("type the number: "))
+        #subtract the previously chosen weapon stats from character stats
+        self.inv.append(weapontype[choice-1])
+        self.phydmg+=weapontype[choice-1]['phydmg']
+        self.rngdmg+=weapontype[choice-1]['rngdmg']
+        self.mgcdmg+=weapontype[choice-1]['mgcdmg']
+        print(f"Current Inventory: {self.inv}")
+
+    def smithy(self):
+        upgradecounter=0
+        if self.equipped['name']==weapontype[0]['name']:
+            print("if you wanna upgrade your current weapon you will need enough resources: 10 metal")
+    #def genstore():
+
+self= shop([])
