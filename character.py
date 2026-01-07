fa99cc0ff33a1abfd9ce9086c044982ab99f2186
RichardY42 <RichardY42@nycstudents.net>
2026-01-07 09:53:34 -0500

    a

--- character.py
+++ character.py
@@ -51,39 +51,40 @@ class Class:
 self = Class("John", 1, 1, 1, 0, 0, 0, [], [], [])
 
 while True:
-    user_input = input("Please choose a race!: Human, Demon, Angel, Dwarf - Remember you will not be able to change these later on. Keep in mind each race will have their own buffs/debuffs. If you would like to get info about these races, please type 'info'")
+    user_input = input("Please choose a race!: Human, Demon, Angel, Dwarf - Remember you will not be able to change these later on. Keep in mind each race will have their own buffs/debuffs. If you would like to get info about these races, please type 'info'").lower()
     if user_input == "info":
         print("The human class, the jack of all trades - master of none. This race is capable of becoming all of the classes, and poses no debuffs. All stats of this race are default. The demon class is a master of combat, specializing in close ranged physical fights. The demon class has high damage and a special ability, but sacrifices some health for it. Demon is incapable of using ranged weapons. The angel race is similar to the human race, but has a few passives and abilities. The angel is capable of using its wings to dodge attacks and is eligible to use all types of weapons and is stronger with magic type attacks. The dwarf race specializes in ranged combat, being small enough to evade attacks. The dwarf class is weaker with physical weapons, and stronger with ranged weapons.")
-    elif user_input == "Human":
+    elif user_input == "human":
         self.human()
         break
-    elif user_input == "Demon":
+    elif user_input == "demon":
         self.demon()
         break
-    elif user_input == "Angel":
+    elif user_input == "angel":
         self.angel()
         break
-    elif user_input == "Dwarf":
+    elif user_input == "dwarf":
         self.dwarf()
         break
     else:
         print("Answer not reconized, please try again.")
 
 while True:
-    user_input = input("Please choose a class for your character, remember that some classes wont be capable of becoming specific classes. - Warrior - Archer - Mage -")
-    if user_input == "Warrior":
+    user_input = input("Please choose a class for your character, remember that some classes wont be capable of becoming specific classes. - Warrior - Archer - Mage -").lower()
+    if user_input == "warrior":
         self.Warrior()
         print("Class chosen: Warrior")
         break
-    elif user_input == "Archer" and self.race != ["Demon"]:
+    elif user_input == "archer" and self.race != ["Demon"]:
         self.Archer()
         print("Class Chosen: Archer")
         break
-    elif user_input == "Mage" and self.race != ["Demon"]:
+    elif user_input == "mage" and self.race != ["Demon"]:
         self.Mage()
         print("Class Chosen: Mage")
         break
 
+#_________________________________________________________________________________________
 weapontype=[
     {
         "name": "rusty sword",
@@ -131,10 +132,11 @@ resources=[
 
 #I FORGOT HOW TO GET THE [CLASS] class FROM CHARACTER.PY AHHHHHHHHHH
 
-class shop(Class):
+class shop():
     def __init__(self, equipped):
         super().__init__(self)
         self.equipped=equipped
+        self= shop([])
     def weaponshop(self):
         print("Buy a weapon... but choose wisely:")
         for index, i in enumerate(weapontype, start=1): 
@@ -153,4 +155,9 @@ class shop(Class):
             print("if you wanna upgrade your current weapon you will need enough resources: 10 metal")
     #def genstore():
 
-self= shop([])
+
+
+while True:
+    user_input = input("x")
+    if user_input== "x":
+        self.weaponshop()
